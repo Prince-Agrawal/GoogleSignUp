c105362f9a09cb73d54c689d7d00a8d442a793ff
Utkarsh Agrawal <utkarshagrawal2022@gmail.com>
2020-05-14 21:55:52 +0530

    ModelWork

--- models/user-model.js
+++ models/user-model.js
@@ -1,11 +1,68 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    username: String,
-    googleId: String
+const freelancerRecruiterSchema = new Schema({
+        username: {
+            type : Schema.Types.String
+        },
+        googleId:{
+            type : Schema.Types.String
+        }, 
+        register_date: {
+            type: Schema.Types.Date,
+        },
+        
+        email: {
+            type: Schema.Types.String,
+        },
+        password: {
+            type: Schema.Types.String,
+        },
+        profile_type: {
+            type: Schema.Types.String,
+        },
+        experience_details: {
+            type: Schema.Types.String,
+            default: "",
+        },
+        domains: {
+            type: [Schema.Types.String],
+            default: [],
+        },
+        hours: {
+            type: Schema.Types.String,
+            default: "",
+        },
+        locations: {
+            type: [Schema.Types.String],
+            default: [],
+        },
+        acc_settings: {
+            type: String,
+            default: "",
+        },
+        targets: {
+            type: String,
+            default: "",
+        },
+        candidates: {
+            type: [Schema.Types.ObjectId],
+            ref: "candidates",
+            default: [],
+        },
+        assigned_jobs: {
+            type: [Schema.Types.ObjectId],
+            ref: "assigned_jobs",
+            default: [],
+        },
+        payment_details: {
+            type: String,
+            default: "",
+        },
 });
 
-const User = mongoose.model('user', userSchema);
+  
+
+const User = mongoose.model('freelancer_recruiter', freelancerRecruiterSchema);
 
 module.exports = User;
